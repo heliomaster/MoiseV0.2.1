16e17a5126c4238a31f88c84ee9b799f7ae4b434
heliomaster <luigidalmaso@gmail.com>
2021-03-04 15:31:58 +0100

    New try calc to manage sql

--- tryCalc.py
+++ tryCalc.py
@@ -1,17 +1,36 @@
+from datetime import datetime
+
 from DB import *
 import sqlite3
 import pandas as pd
 
 print('marche')
 
-class calculateFromSql():
+
+class CalculateFromSql:
     def __init__(self):
 
         con = sqlite3.connect("LmtPilots.db")
         query = "SELECT * FROM Pilots_hours;"
-        self.df = pd .read_sql_query(query,con, parse_dates=['date_time1', 'date_time2'])
+        self.df = pd .read_sql_query(query, con, parse_dates=['date_time1', 'date_time2'])
         print(f'this is it {self.df}')
 
-bibi = calculateFromSql()
-bibi
 
+
+    # def essai(self):
+
+
+        self.df = pd.read_sql_query(('select "Timestamp","Value" from "Pilots_hours" '
+                             'where "Timestamp" BETWEEN %s AND %s'),query,
+                           params=[datetime(2014,6,24,16,0),datetime(2014,6,24,17,0)],
+                           index_col=['Timestamp'])
+        return self.df
+
+
+
+
+
+
+bibi = CalculateFromSql()
+bibi
+bibi.essai()
